19243810af38ca323ca6306c2a07005270c6d4de
dmitrymailk <dmitrymail_k@mail.ru>
2021-03-07 15:47:58 +0300

    improve maps

--- src/views/HospitalPage.vue
+++ src/views/HospitalPage.vue
@@ -22,6 +22,7 @@ export default {
   data() {
     return {
       map: null,
+      search: null,
       lope,
       first: true,
     };
@@ -49,13 +50,23 @@ export default {
           clickable: true,
         });
         marker.addListener("click", () => {
-          // console.log();
+          console.log(place.photos[0].getUrl());
           let lat = place.geometry.location.lat();
           let lng = place.geometry.location.lng();
           this.map.setCenter({ lat, lng });
 
-          let title = place.name;
-          this.showElement(title);
+          let placeId = place.place_id;
+
+          // let detailsReq = new self.google.maps.places.PlaceDetailsRequest(
+          //   placeId
+          // );
+          this.search.getDetails({ placeId }, (results, status) => {
+            let title = place.name;
+            let url = place.photos[0].getUrl();
+            console.log("DETAILS", results, status);
+            let phone = results.international_phone_number;
+            this.showElement(title, url, phone);
+          });
         });
       });
     },
@@ -67,6 +78,7 @@ export default {
         rankBy: self.google.maps.places.RankBy.DISTANCE,
         keyword: keyword,
       };
+      this.search = search;
       let callback = (results, status) => {
         if (status == self.google.maps.places.PlacesServiceStatus.OK) {
           console.log("RES", results, status);
@@ -78,14 +90,26 @@ export default {
 
       console.log(search, callback);
     },
-    showElement: function(_title) {
+    showElement: function(_title, _imgPath, _phone) {
       const controlDiv = document.createElement("div");
       let title = document.createElement("div");
       title.textContent = _title;
       title.classList.add("hospital__card-title");
 
+      let img_block = document.createElement("div");
+      let img = document.createElement("img");
+      img.src = _imgPath;
+      img_block.appendChild(img);
+      img_block.classList.add("hospital__card-img");
+
+      let phone = document.createElement("div");
+      phone.textContent = _phone;
+      phone.classList.add("hospital__card-phone");
+
       controlDiv.classList.add("hospital__card");
       controlDiv.appendChild(title);
+      controlDiv.appendChild(img_block);
+      controlDiv.appendChild(phone);
 
       //
       if (
@@ -113,7 +137,7 @@ export default {
   width: 326px
   // max-height: 164px
   position: absolute
-  height: 164px
+  height: 193px
   // margin: 60px 0 0 286px
   &-title
     font-family: Quicksand
@@ -123,6 +147,19 @@ export default {
     line-height: 27px
     color: #000000
     margin: 10px 0 0 14px
+  &-img
+    width: 147px
+    height: 98px
+    overflow: hidden
+    border-radius: 8px
+    position: absolute
+    margin: 16px 0 0 166px
+    img
+      height: 100%
+  &-phone
+    position: absolute
+    font-size: 16px
+    margin: 16px 0 0 16px
 
 
 .hospital-page
